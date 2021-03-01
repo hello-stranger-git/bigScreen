2988119fc3959d456828a62ce0450b173d6f21c1
xyc <1365884247@qq.com>
2021-03-01 11:53:44 +0800

    修改客流单价分析柱状图样式

--- src/views/bigScreen/flowPrice/index.vue
+++ src/views/bigScreen/flowPrice/index.vue
@@ -10,18 +10,36 @@ export default {
   data () {
     return {
       option: {
+
         grid: {
-          show: false
+          left: '0%',
+          right: '2%',
+          bottom: '3%',
+          containLabel: true
         },
         xAxis: {
           type: 'category',
-          data: ['01/12', '01/11', '01/10', '01/09', '01/08']
+          data: ['01/12', '01/11', '01/10', '01/09', '01/08'],
+          axisTick: {
+            show: false
+          },
+          axisLabel: {
+            color: '#fff6',
+            fontWeight: 400,
+            fontSize: 14,
+            margin: 14,
+            interval: 0
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#4080fe1f'
+            }
+          }
 
         },
         yAxis: [
           {
             type: 'value',
-            // name: '客流人数',
             min: 0,
             max: 500,
             position: 'left',
@@ -32,14 +50,21 @@ export default {
               }
             },
             axisLabel: {
-              formatter: '{value} '
+              color: '#1184D9',
+              fontWeight: 400,
+              fontSize: 14,
+              showMinLabel: false
             },
-            splitLine: { show: false }
+            splitLine: {
+              show: false,
+              lineStyle: {
+                color: '#4080fe1f'
+              }
+            }
 
           },
           {
             type: 'value',
-            // name: '销售额',
             min: 0,
             max: 1000000,
             position: 'right',
@@ -50,6 +75,10 @@ export default {
               }
             },
             axisLabel: {
+              color: '#1184D9',
+              fontWeight: 400,
+              fontSize: 14,
+              showMinLabel: false,
               formatter: (value) => {
                 if (value > 10000) {
                   return parseInt(value / 10000) + 'w'
@@ -89,25 +118,6 @@ export default {
         },
         series: [
           {
-            name: '客流人数',
-            data: [360, 202, 340, 190, 170],
-            type: 'bar',
-            // showBackground: true,
-            barWidth: '15%',
-            barGap: '50%',
-            itemStyle: {
-              normal: {
-                color: new this.$echarts.graphic.LinearGradient(
-                  0, 0, 0, 1,
-                  [
-                    { offset: 0, color: '#EF2B2D' },
-                    { offset: 1, color: '#FE38A7' }
-                  ]
-                )
-              }
-            }
-          },
-          {
             name: '销售额',
             data: [490000, 170000, 390000, 180000, 160000],
             lineStyle: {
@@ -122,12 +132,32 @@ export default {
             itemStyle: {
               // 圆角
               normal: {
-                // barBorderRadius: [10, 10, 0, 0],
+                barBorderRadius: [10, 10, 0, 0],
                 color: new this.$echarts.graphic.LinearGradient(
                   0, 0, 0, 1,
                   [
                     { offset: 0, color: '#57E6B1' },
                     { offset: 1, color: '#4080FE' }
+                  ]
+                )
+              }
+            }
+          },
+          {
+            name: '客流人数',
+            data: [360, 202, 340, 190, 170],
+            type: 'bar',
+            // showBackground: true,
+            barWidth: '15%',
+            barGap: '50%',
+            itemStyle: {
+              normal: {
+                barBorderRadius: [10, 10, 0, 0],
+                color: new this.$echarts.graphic.LinearGradient(
+                  0, 0, 0, 1,
+                  [
+                    { offset: 0, color: '#EF2B2D' },
+                    { offset: 1, color: '#FE38A7' }
                   ]
                 )
               }
@@ -166,8 +196,8 @@ export default {
       this.option.series[1].data = []
       this.option.series[2].data = []
       for (let i = 0; i < 5; i++) {
-        this.option.series[0].data.push(Math.floor(Math.random() * 500 + 0))
-        this.option.series[1].data.push(Math.floor(Math.random() * 1000000 + 0))
+        this.option.series[0].data.push(Math.floor(Math.random() * 1000000 + 0))
+        this.option.series[1].data.push(Math.floor(Math.random() * 500 + 0))
         this.option.series[2].data.push(Math.floor(Math.random() * 500 + 0))
       }
     }, 1500)
